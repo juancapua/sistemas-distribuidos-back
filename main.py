ae0ae438404d910969309c5c78a14aa64a3c9939
francisco <fracachon@gmail.com>
2023-05-23 12:46:26 -0300

    New fields added

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI, File, UploadFile, Form
 from typing import List
 from fastapi.middleware.cors import CORSMiddleware
 import boto3
-from config.db import conn
-from models.Cabin import cabin
+#from config.db import conn
+#from schemas.Cabin import cabin
+
+# source myenv/bin/activate
+# uvicorn main:app --reload
 
 app = FastAPI()
 
@@ -23,8 +26,8 @@ app.add_middleware(
 
 # Datos de ejemplo para la lista de usuarios
 users = [
-    {"id": 1, "name": "Francisco cachon", "image": "https://sistemas-distribuidos-users.s3.amazonaws.com/cachimba.jpeg"},
-    {"id": 2, "name": "Dios del fuchibol","image": "https://sistemas-distribuidos-users.s3.amazonaws.com/leo-perfil-2.webp"}
+    {"id": 1, "name": "Francisco cachon", "beds": 3, "price": 2500, "image": "https://sistemas-distribuidos-users.s3.amazonaws.com/cachimba.jpeg"},
+    {"id": 2, "name": "Dios del fuchibol", "beds": 2, "price": 1900, "image": "https://sistemas-distribuidos-users.s3.amazonaws.com/leo-perfil-2.webp"}
 ]
 
 # Endpoint para obtener la lista de usuarios
@@ -33,14 +36,14 @@ async def get_users() -> List[dict]:
     return users
 
 @app.post("/upload")
-async def upload_image(name: str = Form(...), image: UploadFile = File(...)):
+async def upload_image(name: str = Form(...), beds: int = Form(...), price: int = Form(...), image: UploadFile = File(...)):
     # Guardar la imagen en Amazon S3
     s3.upload_fileobj(image.file, 'sistemas-distribuidos-users', image.filename)
     url = s3.generate_presigned_url('get_object', Params={'Bucket': 'sistemas-distribuidos-users', 'Key': image.filename}, ExpiresIn=3600)
-    users.append({"id": users[-1]['id']+1, "name": name, "image": url})
-
+    # Persistir la cabana
+    users.append({"id": users[-1]['id']+1, "name": name, "beds": beds, "price": price, "image": url})
     return {"message": "Imagen cargada correctamente"}
 
-@app.get("/cabins")
-async def get_cabins() -> List[dict]:
-    return conn.execute(cabin.select()).fetchall()
+# @app.get("/cabins")
+# async def get_cabins() -> List[dict]:
+#     return conn.execute(cabin.select()).fetchall()
